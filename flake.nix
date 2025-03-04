53cbc0bcfe8025248e7d77250449730cf93f7a98
Alex David <alexdavid@users.noreply.github.com>
2025-03-04 13:37:52 -0800

    Remove nixpkgs input (#6)
    The `nixpkgs` dependency was only being used for library functions, and
    these functions are only needed within `garnixModules.default` which
    receives `lib` from `garnix-lib`. By removing the `nixpkgs` input we can
    be assured that there is only one `nixpkgs` being used: the one passed
    in from `garnix-lib`.

--- flake.nix
+++ flake.nix
@@ -5,81 +5,88 @@
     [Documentation](https://garnix.io/docs/modules/user) - [Source](https://github.com/garnix-io/user-module).
   '';
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-
   outputs =
-    { self
-    , nixpkgs
-    ,
-    }:
-    let
-      lib = nixpkgs.lib;
-
-      userSubmodule.options = {
-        user = lib.mkOption
-          {
-            type = lib.types.nonEmptyStr;
-            description = "The Linux username.";
-            example = "alice";
-          } // { name = "user name"; };
-
-        groups = lib.mkOption {
-          type = lib.types.listOf lib.types.str;
-          description = "The groups the user belongs to.";
-          example = [ "wheel" ];
-          default = [ ];
-        };
-
-        shell = lib.mkOption {
-          type = lib.types.enum [ "bash" "zsh" "fish" ];
-          default = "bash";
-          description = "The users login shell.";
-        };
-
-        authorizedSshKeys = lib.mkOption
-          {
-            type = lib.types.listOf lib.types.nonEmptyStr;
-            description =
-              ''The public SSH keys that can be used to log in as this user. (Note that you must
-            use the IP address rather than domain for SSH.)'';
-          } // { name = "SSH keys"; };
-      };
-    in
+    { self }:
     {
-      garnixModules.default = { pkgs, config, ... }: {
-        options = {
-          user = lib.mkOption {
-            type = lib.types.attrsOf (lib.types.submodule userSubmodule);
-            description = "An attrset of users.";
+      garnixModules.default =
+        {
+          pkgs,
+          lib,
+          config,
+          ...
+        }:
+        let
+          userSubmodule.options = {
+            user =
+              lib.mkOption {
+                type = lib.types.nonEmptyStr;
+                description = "The Linux username.";
+                example = "alice";
+              }
+              // {
+                name = "user name";
+              };
+
+            groups = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              description = "The groups the user belongs to.";
+              example = [ "wheel" ];
+              default = [ ];
+            };
+
+            shell = lib.mkOption {
+              type = lib.types.enum [
+                "bash"
+                "zsh"
+                "fish"
+              ];
+              default = "bash";
+              description = "The users login shell.";
+            };
+
+            authorizedSshKeys =
+              lib.mkOption {
+                type = lib.types.listOf lib.types.nonEmptyStr;
+                description = ''
+                  The public SSH keys that can be used to log in as this user. (Note that you must
+                              use the IP address rather than domain for SSH.)'';
+              }
+              // {
+                name = "SSH keys";
+              };
+          };
+        in
+        {
+          options = {
+            user = lib.mkOption {
+              type = lib.types.attrsOf (lib.types.submodule userSubmodule);
+              description = "An attrset of users.";
+            };
+          };
+
+          config = {
+            nixosConfigurations.default = builtins.attrValues (
+              builtins.mapAttrs (name: projectConfig: {
+                users.users.${projectConfig.user} = {
+                  extraGroups = projectConfig.groups;
+                  isNormalUser = true;
+                  shell = pkgs.${projectConfig.shell};
+                  openssh.authorizedKeys.keys = projectConfig.authorizedSshKeys;
+                };
+                programs.zsh.enable = projectConfig.shell == "zsh";
+                programs.fish.enable = projectConfig.shell == "fish";
+                services.openssh = {
+                  enable = true;
+                  settings = {
+                    PasswordAuthentication = false;
+                    KbdInteractiveAuthentication = false;
+                    AuthenticationMethods = "publickey";
+                    PermitRootLogin = "prohibit-password";
+                  };
+                };
+              }) config.user
+            );
           };
         };
-
-        config =
-          {
-            nixosConfigurations.default =
-              builtins.attrValues (builtins.mapAttrs
-                (name: projectConfig: {
-                  users.users.${projectConfig.user} = {
-                    extraGroups = projectConfig.groups;
-                    isNormalUser = true;
-                    shell = pkgs.${projectConfig.shell};
-                    openssh.authorizedKeys.keys = projectConfig.authorizedSshKeys;
-                  };
-                  programs.zsh.enable = projectConfig.shell == "zsh";
-                  programs.fish.enable = projectConfig.shell == "fish";
-                  services.openssh = {
-                    enable = true;
-                    settings = {
-                      PasswordAuthentication = false;
-                      KbdInteractiveAuthentication = false;
-                      AuthenticationMethods = "publickey";
-                      PermitRootLogin = "prohibit-password";
-                    };
-                  };
-                })
-                config.user);
-          };
-      };
     };
 }
-
